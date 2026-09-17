8347ee3c8c98726195b7f22e7d593d4312c84819
Mercury-07 <grahamhudson84@gmail.com>
2026-09-17 11:37:13 -0400

    Create BlackoutModifier.cs

---
+++ TownOfUs/Modifiers/Game/Impostor/BlackoutModifier.cs
@@ -0,0 +1,7 @@
+/*
+Blackout: activate anonymous voting once per game
+
+Ability (in meeting): Anonomyize - Activates anonymous voting lol
+
+Technical Issues: 
+- Blackout cannot be pulled if anonymous voting is already on
