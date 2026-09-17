e101c32ee5f63211249ddaf4fa560a51dd203e7a
Mercury-07 <grahamhudson84@gmail.com>
2026-09-17 11:32:25 -0400

    Planned TechnicianRole.cs

--- TownOfUs/Roles/Classic/Crewmate/CrewmateSupport/TechnicianRole.cs
+++ TownOfUs/Roles/Classic/Crewmate/CrewmateSupport/TechnicianRole.cs
@@ -1,1 +1,26 @@
 /*
+Technician : Reset Cooldowns to empower the crew!
+
+Ability: RESET - Resets all cooldowns on target
+- Toggle if RESET affects modifiers - True by Default
+- Choose Percentage of KILL reset on ability use - 100% by default
+- Toggle if RESET can be used on roles without a direct ability - False by default
+- Toggle if RESET can be used on roles with an ability already primed - False by default
+- Toggle if players are notified that RESET has been used on them - False by default
+- Choose Delay of RESET being applied to the target - Default is 0 seconds
+- Choose Cooldown of RESET - Default is 15 seconds
+- Toggle if Technician can interact with Time Lord - False by Default  (softlock prevention)
+- Toggle if RESET gives uses to abilities out of charges (Engineer Fix, Veteran Alert, etc) - True by Default
+
+Playstyle: 
+- Technician can silently role reveal to most players, encouraging them to stick together and continously boost
+  their buddy. However, they may accidentally be powering Imposter abilities like Escapist or Janitor, allowing them to 
+  discretely kill at a much faster pace
+- Lovers becomes VERY powerful with Technician
+- Technician is able to be detected by roleguessers if they notice visible abilities being used too often
+
+Technical Issues: 
+- Time Lord Toggle might be weird
+- Targeting could get mixed up
+
+*/
